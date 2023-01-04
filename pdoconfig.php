28f281f427f13c2968e4480d53971c6784ab8f6b
Diagolis <bastien.lopez40@gmail.com>
2023-01-04 10:59:03 +0100

    Create pdoconfig.php

---
+++ pdoconfig.php
@@ -0,0 +1,14 @@
+<?php
+	$host = '127.0.0.1';
+	$dbname = 'projet1';
+	$username = 'root';
+	$password = 'root';
+
+	
+/* log lycée
+	$host = '127.0.0.1';
+	$dbname = 'projet';
+	$username = 'Serveur_P3';
+	$password = '3r3tLz7';
+*/
+?>
